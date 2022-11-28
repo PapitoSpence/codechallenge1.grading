b134d7b29d857e4864d2d886932bacbfc48e5992
PapitoSpence <PapitoSpence>
2022-11-28 16:02:19 +0300

    complete

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const prompt = require('prompt-sync')();
 const grade =parseInt(prompt('Enter a value between o and 100:'));
 function nameGrading () {
-  
+
  // Nested if else
  if (grade  >= 79 && grade <= 100) {
     console.log('A');
-;}
- if (grade >= 60 && grade <= 78) {
+}
+else if (grade <= 79 && grade >= 60) {
         console. log('B');
-    };
-if (grade >= 50 && grade <= 59) {
+    }
+else if (grade <= 59 && grade >= 49) {
             console. log('C');
         }
-        else if (grade >= 40 && grade <= 49) {
+        else if (grade <= 49 && grade >= 40) {
                 console. log('D');
             }
-            else if (grade < 39 && grade <=0 ){
+            else if (grade < 40 && grade >0 ){
                 console.log('E')
             }  
 
             else {  
-                console.log('Error');
+                console.log('Error')
                 
             }
-            nameGrading()
+        
 }
+nameGrading();
